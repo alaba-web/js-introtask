ed44c794b2536ed94158f11aff3c8907c85feaa7
unknown <aminualaba9@gmail.com>
2022-06-22 11:50:16 +0100

    initial commit

---
+++ zuri.js
@@ -0,0 +1,7 @@
+document.getElementById('name').innerHTML = 'Alaba Azeez AMINU';
+
+
+document.getElementsByClassName('height').innerHTML = 'My height is 610ft';
+
+
+document.getElementsByTagName('h3')[0].innerHTML = 'I am a Nigerian';
